812509e6b4d7e4135f6e63c4b31a09c37324b059
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2025-02-18 20:07:16 -0300

    Criação de um Aside com minhas redes sociais na lateral das páginas.

---
+++ src/componentes/asideRedesSociais/RedesLaterais.jsx
@@ -0,0 +1,24 @@
+import { FaLinkedin } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io";
+
+function RedesLaterais() {
+
+    return (
+
+        <div className="w-[4%] fixed top-[40%] md:w-[6%] md:top-[42%] sm:w-[8%] sm:mr-[50%] sm:top-[42%] max-sm:w-[12%]  max-sm:top-[45%] bg-gray-200 shadow-xl rounded-s-lg " dir="rtl">
+
+                <aside className="flex flex-col items-center p-4 sm:p-3 max-sm:p-2 gap-5">
+
+                    <a href="#" className="hover:opacity-75"> <FaLinkedin className="sm:text-3xl max-sm:text-2xl"/> </a>
+                    <a href="#" className="hover:opacity-75"> <IoLogoGithub className="sm:text-3xl max-sm:text-2xl"/> </a>
+
+
+                </aside>
+
+        </div>
+
+    )
+
+}
+
+export default RedesLaterais;
--- src/componentes/paginaInicial/PaginaInicial.jsx
+++ src/componentes/paginaInicial/PaginaInicial.jsx
@@ -1,4 +1,5 @@
 import Header from "../header/Header";
+import RedesLaterais from "../asideRedesSociais/RedesLaterais";
 
 function PaginaInicial () {
 
@@ -9,6 +10,10 @@ function PaginaInicial () {
             <Header/>
             <main className="w-full">
 
+                <div className="relative">
+                    <RedesLaterais/>
+                </div>
+
                 <div className="w-[80%] m-auto">
 
                     <section>
@@ -17,7 +22,7 @@ function PaginaInicial () {
                             <h1 className="text-5xl font-bold font-serif break-words max-sm:text-4xl">Bem-vindo!</h1>
                         </div>
 
-                        <div className="w-[100%] m-auto">
+                        <div className="w-[80%] m-auto">
                             <p className="text-lg font-serif break-words">Olá! Sou Valter dos Santos Filho, Desenvolvedor de Sistemas Web, com foco nas tecnologias: HTML, CSS, JavaScript e MySQL, e este é o meu portifólio. Aqui você encontrará informações sobre mim, meus projetos e como entrar em contato comigo.</p>	
                             <p className="text-lg font-serif max-sm:mt-[3%] sm:mt-[2%]">Fique à vontade para navegar pelo site e conhecer um pouco mais sobre o meu trabalho.</p>
                         </div>
