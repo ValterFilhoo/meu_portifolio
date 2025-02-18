01bdf4d76baa297b756c435f210ec63dde924fa8
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2025-02-18 15:34:58 -0300

    excluido o dist novamente

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/meu_portifolio/', // Nome do repositório no GitHub
+  base: '/meu_portifolio/',
 })
