1574725c9da7049e079951acdfd1d58c49e9b3a7
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2025-03-11 09:37:06 -0300

    Atualizão do posicionamento do menu de categorias no mobile.

--- src/componentes/paginaInicial/PaginaInicial.jsx
+++ src/componentes/paginaInicial/PaginaInicial.jsx
@@ -23,8 +23,8 @@ function PaginaInicial () {
                         </div>
 
                         <div className="w-[80%] m-auto">
-                            <p className="text-lg font-serif break-words">Olá! Sou Valter dos Santos Filho, Desenvolvedor de Sistemas Web, com foco nas tecnologias: HTML, CSS, JavaScript e MySQL, e este é o meu portifólio. Aqui você encontrará informações sobre mim, meus projetos e como entrar em contato comigo.</p>	
-                            <p className="text-lg font-serif max-sm:mt-[3%] sm:mt-[2%]">Fique à vontade para navegar pelo site e conhecer um pouco mais sobre o meu trabalho.</p>
+                            <p className="text-lg text-justify font-serif break-words">Olá! Sou Valter dos Santos Filho, Desenvolvedor de Sistemas Web, com foco nas tecnologias: HTML, CSS, JavaScript e MySQL, e este é o meu portifólio. Aqui você encontrará informações sobre mim, meus projetos e como entrar em contato comigo.</p>	
+                            <p className="text-lg text-justify font-serif max-sm:mt-[3%] sm:mt-[2%]">Fique à vontade para navegar pelo site e conhecer um pouco mais sobre o meu trabalho.</p>
                         </div>
 
                     </section>
