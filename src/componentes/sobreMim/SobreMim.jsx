b5aea94b61d85826754399c9d3d53eb698d3ef0d
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2025-03-11 12:01:28 -0300

    Aumentando o tamanho do container dos paragrafos na página Sobre Mim para 85%.

--- src/componentes/sobreMim/SobreMim.jsx
+++ src/componentes/sobreMim/SobreMim.jsx
@@ -18,11 +18,11 @@ function SobreMim() {
 
                 {/*Titulo Principal */}
                 <div className="flex justify-center w-full mt-20 mb-10">
-                    <h1 className="text-5xl text-center border-2 font-bold font-serif break-words max-sm:text-4xl">Sobre Mim</h1>
+                    <h1 className="text-5xl text-center font-bold font-serif break-words max-sm:text-4xl">Sobre Mim</h1>
                 </div>
 
                 {/*Parágro com as informações resumidas do tópico Sobre Mim. */}
-                <div className="w-[80%] m-auto">
+                <div className="w-[85%] m-auto">
                     <p className="text-lg text-justify font-serif break-words">Olá! Sou Valter dos Santos Filho, desenvolvedor de sistemas web e estudante de Análise e Desenvolvimento de Sistemas no Instituto Federal Baiano (Campus Catu), localizado em Catu, Bahia. Estou atualmente no quinto semestre do curso, onde venho adquirindo conhecimento e habilidades em diversas áreas do desenvolvimento de software.</p>
                 </div>
 
@@ -32,7 +32,7 @@ function SobreMim() {
                 </div>
 
                 {/*Parágrafo relacionado a formação academica */}
-                <div className="w-[80%] m-auto">
+                <div className="w-[85%] m-auto">
                     <p className="text-lg text-justify font-serif break-words">Atualmente, estou cursando Análise e Desenvolvimento de Sistemas no Instituto Federal Baiano (IF Baiano), Campus Catu. Durante minha jornada acadêmica, tive a oportunidade de participar como competidor no evento Robocup 2023 em Salvador-Bahia ao longo de 5 dias, sendo uma experiência que foi extremamente enriquecedora e me proporcionou um aprendizado prático valioso.</p>
                 </div>
 
@@ -40,11 +40,11 @@ function SobreMim() {
                     <h2 className="text-4xl text-center font-bold font-serif break-words max-sm:text-3xl">Experiência Profissional e Acadêmica</h2>
                 </div>
 
-                <div className="w-[80%] m-auto">
+                <div className="w-[85%] m-auto">
                     <p className="text-lg text-justify font-serif break-words">Estou em busca da minha primeira oportunidade profissional na área de desenvolvimento. No entanto, já tive a chance de trabalhar em vários projetos acadêmicos e pessoais que me permitiram aplicar e expandir meus conhecimentos:</p>
                 </div>
 
-                <div className="w-[80%] m-auto">
+                <div className="w-[85%] m-auto">
                     
                     <ul className="list-inside text-lg font-serif break-words">
                         
@@ -54,7 +54,7 @@ function SobreMim() {
 
                                 <ul className="list-disc list-inside text-lg font-serif break-words">
 
-                                    <li className="ml-20">Desenvolvi um protótipo de portal de notícias usando Figma. Inicialmente implementado com HTML e CSS, estou atualmente refazendo-o com React.js e Express.js.</li>
+                                    <li className="ml-10 text-justify">Desenvolvi um protótipo de portal de notícias usando Figma. Inicialmente implementado com HTML e CSS, estou atualmente refazendo-o com React.js e Express.js.</li>
 
                                 </ul>
 
@@ -66,7 +66,7 @@ function SobreMim() {
 
                                 <ul className="list-disc list-inside text-lg font-serif break-words">
 
-                                    <li className="ml-20">Participei da criação de um site de vendas de acessórios, utilizando HTML, CSS, JavaScript, PHP e MySQL.</li>
+                                    <li className="ml-10 text-justify">Participei da criação de um site de vendas de acessórios, utilizando HTML, CSS, JavaScript, PHP e MySQL.</li>
 
                                 </ul>
 
@@ -78,7 +78,7 @@ function SobreMim() {
 
                             <ul className="list-disc list-inside text-lg font-serif break-words">
 
-                                <li className="ml-20">Estou desenvolvendo um aplicativo Android com detecção de objetos orientado a comandos de voz personalizados, utilizando Kotlin, para auxiliar pessoas cegas e de baixa visão.</li>
+                                <li className="ml-10 text-justify">Estou desenvolvendo um aplicativo Android com detecção de objetos orientado a comandos de voz personalizados, utilizando Kotlin, para auxiliar pessoas cegas e de baixa visão.</li>
 
                             </ul>
 
@@ -91,7 +91,7 @@ function SobreMim() {
 
                                 <ul className="list-disc list-inside text-lg font-serif break-words">
 
-                                    <li className="ml-20">Participei da implementação de dois sistemas web, sendo um utilizando HTML, CSS, JS, PHP e MySQL, e o outro fazendo uso das mesmas tecnologias, mas aplicando padrões de projetos.</li>
+                                    <li className="ml-10 text-justify">Participei da implementação de dois sistemas web, sendo um utilizando HTML, CSS, JS, PHP e MySQL, e o outro fazendo uso das mesmas tecnologias, mas aplicando padrões de projetos.</li>
 
                                 </ul>
 
@@ -107,8 +107,8 @@ function SobreMim() {
                     <h2 className="text-4xl text-center font-bold font-serif break-words max-sm:text-3xl">Interesses e Objetivos de Carreira</h2>
                 </div>
 
-                <div className="w-[80%] m-auto">
-                    <p className="text-lg text-justify font-serif break-words">Desejo atuar como desenvolvedor full-stack ou mobile, ainda estou explorando minhas opções para decidir qual caminho seguir. A curto prazo, meu objetivo é conseguir minha primeira oportunidade profissional e começar a desenvolver uma carreira consistente. A longo prazo, pretendo continuar evoluindo e me especializando, contribuindo para projetos inovadores que façam a diferença na vida das pessoas.</p>
+                <div className="w-[85%] m-auto">
+                    <p className="text-lg text-justify font-serif">Desejo atuar como desenvolvedor full-stack ou mobile, ainda estou explorando minhas opções para decidir qual caminho seguir. A curto prazo, meu objetivo é conseguir minha primeira oportunidade profissional e começar a desenvolver uma carreira consistente. A longo prazo, pretendo continuar evoluindo e me especializando, contribuindo para projetos inovadores que façam a diferença na vida das pessoas.</p>
                 </div>
 
                 {/*Titulo secundário */}
@@ -119,12 +119,12 @@ function SobreMim() {
                 <div className="w-[80%] m-auto">
 
                     <ul className="list-disc text-lg font-serif break-words">
-                        <li>Desenvolvimento Web: HTML, CSS, JavaScript, PHP, MySQL, React.js e Tailwind; </li>
-                        <li>Desenvolvimento Mobile: Kotlin;</li>
-                        <li>UI/UX Design: Princípios e práticas de design de interfaces;</li>
-                        <li>Metodologias Ágeis: Conhecimento teórico de SCRUM e XP;</li>
-                        <li>Banco de Dados: MySQL;</li>
-                        <li>Orientação a Objetos: Conhecimento teórico e aplicação prática.</li>
+                        <li className="mb-3">Desenvolvimento Web: HTML, CSS, JavaScript, PHP, MySQL, React.js e Tailwind; </li>
+                        <li className="mb-3">Desenvolvimento Mobile: Kotlin;</li>
+                        <li className="mb-3">UI/UX Design: Princípios e práticas de design de interfaces;</li>
+                        <li className="mb-3">Metodologias Ágeis: Conhecimento teórico de SCRUM e XP;</li>
+                        <li className="mb-3">Banco de Dados: MySQL;</li>
+                        <li className="mb-3">Orientação a Objetos: Conhecimento teórico e aplicação prática.</li>
                     </ul>
 
                 </div>
